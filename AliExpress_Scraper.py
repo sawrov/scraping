e34a23187a8db4008ab8ea7acff17f08c6282aa7
sawrov <sawrov@hotmail.com>
2020-07-07 13:33:12 +0800

    updated the class draft 1.1 ---

--- AliExpress_Scraper.py
+++ AliExpress_Scraper.py
@@ -14,8 +14,9 @@ import requests.exceptions as reqexception
 
 
 class AliExpressScraper:
-    url = ""
+    current_url = ""
     list_of_url = ""
+    information = {}
 
     def __init__(self):
         print("-----------------INITIALIZING SCRAPER--------------\n")
@@ -36,17 +37,19 @@ class AliExpressScraper:
             exit()
 
     def update_url(self, url):
-        try:
-            response = request.urlopen(url).getcode()
-            if response == 200:
-                self.url = url
-                return True
-            else:
-                print("\nPlease Check The Url")
+        if url:
+            try:
+                response = request.urlopen(url).getcode()
+                if response == 200:
+                    self.current_url = url
+                    return True
+                else:
+                    print("\nPlease Check The Url")
+                    return False
+            except reqexception.ConnectionError:
+                print("\nCHECK INTERNET CONNECTION")
                 return False
-        except reqexception.ConnectionError:
-            print("\nCHECK INTERNET CONNECTION")
-            return False
+
 
     def read_url_from_file(self, file):
         try:
@@ -57,12 +60,46 @@ class AliExpressScraper:
             print("\n\n" + file + "-----NOT FOUND")
             return False
 
+    def load_url(self):
+        try:
+            self.driver.get(self.current_url)
+            _ = self.driver.find_element_by_xpath('//*[@id="root"]')
+            try:
+                wait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "next-dialog-body")))
+                self.driver.find_element_by_class_name("next-dialog-close").click()
+            except DriverExceptions.NoSuchElementException:
+                print("No POP-UP Detected")
+                pass
+            return True
+        except DriverExceptions.NoSuchElementException:
+            print("THE URL ENTERED IS NOT COMPATIBLE WITH THE PROGRAM\n")
+            print("PLEASE ENTER A VALID URL")
+            print("ANY ALI-EXPRESS PAGE TO BUY A PRODUCT IS VALID \n")
+            return False
+
+    def update_basic_information(self):
+        self.information["price"] = self.driver.find_element_by_class_name('product-price-value')
+        self.information["title"] = self.driver.find_element_by_class_name('product-title-text')
+        self.information["store"] = self.driver.find_element_by_class_name('store-name')
+        self.information["images"] = self.driver.find_elements_by_xpath('//div[@class="images-view-item"]/img')
+        self.information["qty"] = self.driver.find_element_by_class_name("product-quantity-tip")
+
+    def show_info(self):
+        print(self.information["qty"].text)
+        print(self.information["price"].text)
+        print(self.information["store"].text)
+        print(self.information["title"].text)
+
     def terminate(self):
         self.driver.quit()
 
-
-
+test=input("Enter URL to scrape : ")
 scrape = AliExpressScraper()
-scrape.read_url_from_file("aliexpress34url.txt")
-scrape.update_url("https://stackoverflow.com/questions/1949318/checking-if-a-website-is-up-via-python")
+scrape.read_url_from_file(test)
+if scrape.update_url(test):
+    if scrape.load_url():
+        scrape.update_basic_information()
+        scrape.show_info()
+        print("-----------THE SCRAPING COMPLETED SUCCESSFULLY----------\n\n")
+
 scrape.terminate()
