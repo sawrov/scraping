4f28116e45d8878cafdb7ea24069c640f70d26e6
sawrov <sawrov@hotmail.com>
2020-07-10 13:11:49 +0800

    writing information to a text file

--- AliExpress_Scraper.py
+++ AliExpress_Scraper.py
@@ -32,6 +32,7 @@ class AliExpressScraper:
         self.size_flag = False
         self.description_flag = False
         self.size_color_matrix_flag = False
+        self.output_text = "\t---------------------------------------------------------\n"
         # ----------Flags--------------
         try:
             self.driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -81,6 +82,8 @@ class AliExpressScraper:
             except DriverExceptions.NoSuchElementException:
                 print("No POP-UP Detected")
                 pass
+            finally:
+                self.update_basic_information()
             return True
         except DriverExceptions.NoSuchElementException:
             print("THE URL ENTERED IS NOT COMPATIBLE WITH THE PROGRAM\n")
@@ -89,11 +92,30 @@ class AliExpressScraper:
             return False
 
     def update_basic_information(self):
-        self.information["price"] = self.driver.find_element_by_class_name('product-price-value')
-        self.information["title"] = self.driver.find_element_by_class_name('product-title-text')
-        self.information["store"] = self.driver.find_element_by_class_name('store-name')
+        self.information["price"] = self.driver.find_element_by_class_name('product-price-value').text
+        self.information["title"] = self.driver.find_element_by_class_name('product-title-text').text
+        self.information["store"] = self.driver.find_element_by_class_name('store-name').text
         self.information["images"] = self.driver.find_elements_by_xpath('//div[@class="images-view-item"]/img')
         self.information["qty"] = self.driver.find_element_by_class_name("product-quantity-tip")
+        self.information["shipping_info_element"] = self.driver.find_element_by_class_name("product-shipping")
+
+    def get_description(self):
+        i = 0
+        while i < 5000:
+            try:
+                self.description_element = self.driver.find_element_by_id("product-description")
+            except:
+                pass
+            self.driver.execute_script("window.scrollBy(0, arguments[0]);", i)
+            i = i + 10
+            if self.description_element.text != "":
+                self.description_flag = True
+                break
+
+        if self.description_flag:
+            self.information["description"] = (re.sub(' +', ' ', self.description_element.text))
+        else:
+            self.information["description"] = "UNABLE TO EXTRACT DESCRIPTION: NO DESCRIPTION PRESENT"
 
     def get_variations(self):
         sku_properties = self.driver.find_elements_by_xpath("//div[@class='sku-wrap']/div[@class='sku-property']")
@@ -101,7 +123,6 @@ class AliExpressScraper:
             xpath2 = "//div[@class='sku-wrap']/div[@class='sku-property'][" + str(key + 1) + "]/div"
             element = self.driver.find_element_by_xpath(xpath2)
             xpath3 = "//div[@class='sku-wrap']/div[@class='sku-property'][" + str(key + 1) + "]/ul/li"
-            print(element.text)
             if "Color:" in element.text:
                 self.color_flag = True
                 self.information["track_color_selection"] = self.driver.find_elements_by_xpath(xpath3)
@@ -116,23 +137,25 @@ class AliExpressScraper:
                 self.information["shipping_elements"] = self.driver.find_elements_by_xpath(xpath3)
                 self.information["shipping_details"] = []
                 for ship in self.information["shipping_elements"]:
-                    # self.driver.execute_script("arguments[0].scrollIntoView(true);", ship)
+                    self.driver.execute_script("arguments[0].scrollIntoView(true);",
+                                               self.driver.find_element_by_id("root"))
                     ship.click()
                     _ = wait(self.driver, 10).until(
                         EC.presence_of_element_located((By.XPATH, "//span[@class='product-shipping-info black-link']")))
                     time.sleep(1)
                     self.information["shipping_details"].append(
-                        self.driver.find_element_by_class_name("product-shipping").text)
+                        ship.text + ":->" + self.information["shipping_info_element"].text)
             elif element.text == "Size:":
                 self.information["size_elements"] = self.driver.find_elements_by_xpath(xpath3)
                 self.size_flag = True
                 self.information["size_details"] = []
                 for size in self.information["size_elements"]:
                     self.information["size_details"].append(size.text)
-        if (self.color_flag and self.size_flag):
+        if self.color_flag and self.size_flag:
             self.price_for_size_and_colors()
 
     def price_for_size_and_colors(self):
+
         self.size_color_matrix_flag = True
         self.size_color_matrix = np.zeros(
             shape=(len(self.information["color_elements"]), len(self.information["size_elements"])), dtype=object)
@@ -147,7 +170,7 @@ class AliExpressScraper:
                     break
             for i, color in enumerate(self.information["color_elements"]):
                 if "selected" in self.information["track_color_selection"][i].get_attribute("class"):
-                    print("SELECTED HURRAH")
+                    pass
                 else:
                     color.click()
                 for j, size in enumerate(self.information["size_elements"]):
@@ -162,43 +185,62 @@ class AliExpressScraper:
                     except DriverExceptions.NoSuchElementException:
                         print("Size for " + color.get_attribute("title") + "\tUNAVAILABLE")
                         continue
-            print("VALUE APPENDED TO THE ARRAY IS: \n")
-            print(self.size_color_matrix)
-            self.information["variation_in_size_and_color"][self.index]=(self.size_color_matrix.tolist())
-            print("VALUE RIGHT AFTER ASSIGNMENT: \n")
-            print(self.information["variation_in_size_and_color"][self.index])
+            self.information["variation_in_size_and_color"][self.index] = (self.size_color_matrix.tolist())
             self.index += 1
-        print(self.information["variation_in_size_and_color"])
 
     def show_info(self):
-        print("VALUE AFTER STORING\n")
+        self.get_variations()
+        self.get_description()
+        f = open(str(self.information["title"]) + ".txt", "w+")
+        f.write("NAME:\t" + self.information["title"] + "\n")
+        f.write("STORE:\t" + self.information["store"] + "\n")
+        f.write("BASE-PRICE:\t" + self.information["price"] + "\n")
+        f.write("\nSHIPPING INFORMATION: \n")
         if self.size_color_matrix_flag: print(self.information["variation_in_size_and_color"])
-
-        print("\n")
-
         if self.shipping_flag:
-            print(self.information["shipping_details"])
-        else:
-            print(self.driver.find_element_by_class_name("product-shipping").text)
+            for info in self.information["shipping_details"]:
+                f.write("\t" + info.replace('\r', '').replace('\n', '') + "\n")
 
-        print("\n STORE INFO---\n")
-        print(self.information["store"].text)
-        print(self.information["title"].text)
-        if self.size_color_matrix_flag: print(self.size_color_matrix)
+        else:
+            f.write(self.information["shipping_info_element"])
+
+        if self.color_flag:
+            f.write("\nCOLOR INFORMATION: \n")
+            f.write("\tNumber of Different Colors:" + str(len(self.information["color_details"])))
+            f.write("\n\tColor Names:\n")
+            for color in self.information["color_details"]:
+                f.write("\t\t" + color[0])
+
+        if self.size_flag:
+            f.write("\n\nSIZE INFORMATION: \n")
+            f.write("\tAVAILABLE SIZES: ")
+            for info in self.information["size_details"]: f.write(info + " ")
+
+        if self.size_color_matrix_flag:
+            f.write("\n\nVARIATION IN PRICES:\n")
+            for i, info_list in enumerate(self.information["variation_in_size_and_color"]):
+                if self.shipping_flag: f.write(
+                    "\t FROM:" + self.information["shipping_details"][i].split(":->")[0] + "\n")
+                for j, price in enumerate(info_list):
+                    f.write("\t\t FOR COLOR: " + self.information["color_details"][j][0] + "\n")
+                    for k, l in enumerate(price):
+                        f.write("\t\t\t" + self.information["size_details"][k] + ":" + l.split("||")[0] + " QTY:" +
+                                l.split("||")[1] + "\n")
+        f.write("\n\n---------DESCRIPTION-----------\n\n")
+        f.write(self.information["description"])
 
     def terminate(self):
         self.driver.quit()
 
 
-test=input("Enter URL to scrape : ")
-#test = "https://www.aliexpress.com/item/4001139880092.html?spm=2114.12010612.8148356.3.7d814f04zET2wu"
+test = input("Enter URL to scrape : ")
+# test = "https://www.aliexpress.com/item/4001139880092.html?spm=2114.12010612.8148356.3.7d814f04zET2wu"
 scrape = AliExpressScraper()
 # scrape.read_url_from_file(test)
 if scrape.update_url(test):
     if scrape.load_url():
-        scrape.update_basic_information()
-        scrape.get_variations()
         scrape.show_info()
         print("-----------THE SCRAPING COMPLETED SUCCESSFULLY----------\n\n")
 
 scrape.terminate()
+print("PLEASE CHECK YOUR DIRECTORY FOR TEXT FILES")
