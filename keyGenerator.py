ccd809f8b24cc1bb74ae9e635a1935c667494f7e
sawrov <sawrov@hotmail.com>
2020-07-19 14:16:52 +0800

    key and verify

--- keyGenerator.py
+++ keyGenerator.py
@@ -21,7 +21,7 @@ class Key:
                 if char == check_digit:
                     check_digit_count += 1
                 score += ord(char)
-        if score == 1872 and check_digit_count == 5:
+        if score == 1392 and check_digit_count == 5:
             return True
         return False
 
